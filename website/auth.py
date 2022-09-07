c53c03323d7768891ea34b3792ece47d5e7a26a8
unknown <bandarammohan@gmail.com>
2022-09-07 11:13:38 +0530

    fixed signup issue

--- website/auth.py
+++ website/auth.py
@@ -54,7 +54,7 @@ def sign_up():
             new_user = User(email=email, first_name=firstName, password = generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash("Account Created", category='success')
             return redirect(url_for('views.home'))
 
